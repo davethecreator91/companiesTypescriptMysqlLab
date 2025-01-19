871bc462a0a56da5ecd0b0dce24c5d9e50194207
dbryant <d.bryant718@gmail.com>
2025-01-19 10:40:02 -0500

    connected to mySQL

---
+++ dist/app.js
@@ -0,0 +1,23 @@
+import mysql from 'mysql2/promise';
+import dotenv from "dotenv";
+// const mysql = require('mysql');
+
+dotenv.config();
+
+async function connectToDatabase() {
+    const connection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+    });
+    console.log('Connected to the Database!');
+    const [companies] = await connection.execute('SELECT * FROM companies');
+    const [employees] = await connection.execute('SELECT * FROM employees');
+    console.log(companies, employees );
+    await connection.end();
+};
+connectToDatabase().catch((err) => {
+    console.error('Error connecting to the database:', err);
+});
+//# sourceMappingURL=app.js.map
