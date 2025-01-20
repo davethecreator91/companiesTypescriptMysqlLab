60c3e3a49599505de791969a894987ab70095653
dbryant <d.bryant718@gmail.com>
2025-01-19 21:34:12 -0500

    setup programattically created procedure

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,6 @@
 import mysql from 'mysql2/promise';
 import dotenv from "dotenv";
+import { createPool } from 'mysql2';
 // const mysql = require('mysql');
 
 dotenv.config();
@@ -15,9 +16,38 @@ async function connectToDatabase() {
     const [companies] = await connection.execute('SELECT * FROM companies');
     const [employees] = await connection.execute('SELECT * FROM employees');
     const [employeesCount] = await connection.execute('SELECT COUNT(*) FROM employees');
-
+    const [NewEmployerColumn] = await connection.execute('ALTER TABLE employees ADD COLUMN employer VARCHAR(64)')
+    // const [NewEmployers] = await connection.execute('INSERT INTO employees (employer) VALUES (''))
+    // const newEmployee
     // console.log(companies, employees );
-    console.log(employeesCount)
+    // console.log(employeesCount)
+    console.log(NewEmployerColumn);
+
+    try {
+        const createProcedureSQL = `
+    
+    DELIMITER $$
+    CREATE PROCEDURE add_employee(IN Bill VARCHAR(16))
+    BEGIN
+        INSERT INTO employees (name,email) VALUES ('Bill', 'bill@yahoo.com');
+    END $$
+    DELIMITER;
+    `;
+    await connection.query(createProcedureSQL);
+    console.log('Stored procedure created successfully!');
+    } catch (err) {
+        console.error('Error creating stored procedure:', err);
+    } finally {
+        await createPool.end();
+    }
+
+    createProcedure();
+
+    
+    // \df
+    // call add_employee('Bill')
+    //     )
+
     await connection.end();
 };
 connectToDatabase().catch((err) => {
